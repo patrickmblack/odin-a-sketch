d3a15001b09e00c52f610be46636201702ea4bd6
Patrick <50429647+patrickmblack@users.noreply.github.com>
2023-10-12 15:54:30 -0700

    Generate even grid of squares with specific sides
    Function generates gree with calculated side length
    depending on given value.

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 
-const grid = document.querySelector('#grid-container')
-const square = document.createElement('div')
-
-
 
 main()
 
 
 function main(){
 
-    for (let i = 0; i < 16; i++){
-        const square = document.createElement('div')
-        grid.appendChild(square)
-        console.log('added square')
-    }
-    // generateSquares(4)
+    generateSquares(16)
     // fillSquare()
 }
 
 function generateSquares(sideLength){
+    const gridContainer = document.querySelector('#grid-container')
+    gridContainer.style.gridTemplateColumns = `repeat(${sideLength}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${sideLength}, 1fr)`
+
+    for (let i = 0; i < (sideLength*sideLength); i++){
+        const square = document.createElement('div')
+        square.classList.add('square')
+        gridContainer.appendChild(square)
+        console.log('added square')
+    }
 }
 
 function fillSquare(position){
